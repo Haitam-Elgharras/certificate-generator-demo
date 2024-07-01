152d21a47c077eb59713cef372aada71302c5e81
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-07-01 14:52:54 +0100

    set up project structure

---
+++ src/main/java/com/nttdata/certificatedemo/CertificateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.certificatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificateDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nttdata/certificatedemo/CertificateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.certificatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
