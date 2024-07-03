946292a6f56bf637f8e16bd010defba00eb684bd
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-07-03 11:59:06 +0100

    Add Repository annotation

--- src/main/java/com/nttdata/certificatedemo/dao/repositories/CandidateRepository.java
+++ src/main/java/com/nttdata/certificatedemo/dao/repositories/CandidateRepository.java
@@ -2,6 +2,8 @@ package com.nttdata.certificatedemo.dao.repositories;
 
 import com.nttdata.certificatedemo.dao.entities.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
 }
--- src/main/java/com/nttdata/certificatedemo/dao/repositories/CertificateTemplateRepository.java
+++ src/main/java/com/nttdata/certificatedemo/dao/repositories/CertificateTemplateRepository.java
@@ -2,6 +2,8 @@ package com.nttdata.certificatedemo.dao.repositories;
 
 import com.nttdata.certificatedemo.dao.entities.CertificateTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CertificateTemplateRepository extends JpaRepository<CertificateTemplate, Long>{
 }
